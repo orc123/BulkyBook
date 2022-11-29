62ce951bf0424bea5a266124d4c38348da8033da
Phạm Ngọc Đông <dong.orc12@gmail.com>
2022-11-29 23:34:24 +0700

    add model

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkyBookWeb.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+}
