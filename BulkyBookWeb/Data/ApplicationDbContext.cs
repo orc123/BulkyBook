c4b64c29560c3857be764880e9643faeebace6df
Phạm Ngọc Đông <dong.orc12@gmail.com>
2022-11-30 22:08:22 +0700

    add application db context

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+        public virtual DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>(x =>
+            {
+                x.ToTable("Categories");
+                x.HasKey(x => x.Id);
+                x.Property(t => t.Name).IsRequired(true).HasMaxLength(200);
+            });
+        }
+
+    }
+}
